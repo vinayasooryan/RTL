23fdc2c0d93ba459dc61e21ad26767adbad447db
vinayasooryan <vinayasooryan@gmail.com>
2025-03-14 12:31:15 +0530

    Update mux_2x1_gatelevel.v

--- mux_2x1/gatelevel/mux_2x1_gatelevel.v
+++ mux_2x1/gatelevel/mux_2x1_gatelevel.v
@@ -1,13 +1,13 @@
 
-
 module mux_2_1_gatelevel(
 input d1,d2,
 input s,
-output reg y
+output y
 );
 wire w1,w2,w3;
 and a1(w1,d2,s);
-not
-and a2(
+not n1(w2,s);
+and a2(w3,d1,w2);
+or or1(y,w1,w3);
 
 endmodule
